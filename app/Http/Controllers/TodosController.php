bd1f117bd9521da883c37226032989af3cad0bb6
IsaacEspinozaG <lolomegaisaac@gmail.com>
2023-09-26 22:26:04 -0700

    $todos not recognized

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodosController extends Controller
+{
+    public function store(Request $request) {
+        $request->validate([
+            'title' => 'required|min:3'
+        ]);
+
+        $todo = new Todo;
+        $todo->title = $request->title;
+        $todo->save();
+
+        return redirect()->route('todos')->with('success', 'Tarea creada correctamente');
+    }
+
+    public function index() {
+        $todos = Todo::all();
+        return view('todos.index', ['todos' => $todos]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TodosController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,8 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/todos', function () {
-    return (
-        view('todos.index')
+Route::get('/tareas', function () {
+    return (view('todos.index')
     );
-});
+})->name('todos');
+
+Route::post('/tareas', [TodosController::class, 'index'])->name('todos');
+
+Route::post('/tareas', [TodosController::class, 'store'])->name('todos');
+
+Route::patch('/tareas', [TodosController::class, 'store'])->name('todos-edit');
+
+Route::delete('/tareas', [TodosController::class, 'store'])->name('todos-destroy');
